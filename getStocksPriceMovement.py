9d12eb6446657dd8b987ef383b09fd631af161c4
Ivo <53398611+Dotoks@users.noreply.github.com>
2023-08-28 23:15:44 +0300

    added api limiter, currently not working

--- getStocksPriceMovement.py
+++ getStocksPriceMovement.py
@@ -2,18 +2,20 @@ import finnhub
 import pandas as pd
 import json
 import tabulate
+from ratelimiter import RateLimiter
 from getAllStocks import *
 
 allStocks = getAllStocks()
+rateLimiter = RateLimiter(max_calls=1, period=2)
 for stock in allStocks:
-    stocksData = finnhub_client.company_basic_financials(stock, 'all')
-    stocksData = json.dumps(stocksData, indent=4)
-    with open("stocksData/" + stock + ".json", "w") as stocksDataOutFile:
-        stocksDataOutFile.write(stocksData)
-
+    with rateLimiter:
+        stocksData = finnhub_client.company_basic_financials(stock, 'all')
+        stocksData = json.dumps(stocksData, indent=4)
+        with open("stocksData/" + stock + ".json", "w") as stocksDataOutFile:
+            stocksDataOutFile.write(stocksData)
 
 
 # with open ("stocksData/apple.json", "w") as AppleOutfile:
 #     AppleOutfile.write(appleData)
 # with open ("stocksData/snowflake.json", "w") as snowflakeOutfile:
-#     snowflakeOutfile.write(snowflakeJson)
+#     snowflakeOutfile.write(snowflakeJson)cl
